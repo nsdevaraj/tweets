937ee9a455c2580595da5db77dc9742f6388921b
N.S.Devaraj <nsdevaraj@gmail.com>
2020-01-09 15:07:22 +0530

    update

--- tweets.py
+++ tweets.py
@@ -63,3 +63,32 @@ if oldest != -1:
         sys.exit()  
         
 fp.close()
+
+# get list of users by 100 +list
+
+#api = tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True, compression=True)
+#lst = [
+#        283051440,
+#        277452100,
+#        705431391706681344,
+#        .....
+#        885435593953619969,
+#        706025541531357184]
+#def chunks(lst, n):
+#    """Yield successive n-sized chunks from lst."""
+#    for i in range(0, len(lst), n):
+#        yield lst[i:i + n]
+#        
+#x100x = [lst[i:i + 100] for i in range(0, len(lst), 100)]
+
+#dic = {}
+#for ids in x100x:
+#    user_objs = api.lookup_users(user_ids=ids)
+#    list_of_followers = []
+#    for user in user_objs:
+#        list_of_followers.extend(user.screen_name)
+	
+	
+#dic['screen_name'] = list_of_followers
+#outFile = open('Followers.txt','w')
+#json.dump(dic,outFile,indent=4)
